94fe148d567fd872f5e9b856d2ffd39d5f85655a
Madeline Burns <mburns6023@gmail.com>
2022-11-03 00:31:37 -0400

    debugging

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,104 +1,111 @@
+
+
 var time = moment();
-const hourTotal = 9;
-const startHour = 9;
+const hourTotal = 9; 
+const startHour = 9; 
 
 function divGenerate() {
+
     var olElement = $("<ol>");
     olElement.attr("class", "time-block");
     $('div.container').append(olElement);
 
+
     for (var i = startHour; i < startHour + hourTotal; i++) {
-    var liElement = $("<li>");
-    liElement.attr("class", "row");
-    $('ol').append(liElement);
+        var liElement = $("<li>");
+        liElement.attr("class", "row"); 
+        $('ol').append(liElement);
 
-    var timeString = '';
-    var hour = i % 24;
-    if (hour < 12) {
-        timeString = hour + "AM";
-    }
-     else if (hour == 12) {
-        timeString = hour + "PM";
-     } 
-     else {
-        timeString = (hour - 12) + "PM";
-     }
-
-     var timeP = $("<p>");
-     timeP.attr("class", "col-1 hour py-3");
-     timeP.text(timeString);
-
-     var taskInput = $("<textarea>");
-     taskInput.attr("class", "col-10");
-     taskInput.attr("id", "taskString");
-     taskInput.attr("data-start", "hour");
-    
-     timeP.attr("for", taskInput.attr("id"));
-     taskInput.val(localStorage.getItem(taskInput.attr('id')));
-
-     var saveButton = $('<button>');
-     saveButton.attr("class", "col-1 saveButton");
-     saveButton.html("<h1>SAVE</h1>");
-
-     liElement.append(timeP).append(taskInput).append(saveButton);
-
-     saveButton.click(function(e) {buttonSave(e)});
-
-    }
-}
-
-function changeColor(element, target) {
-    var timeColor = ['past', 'present', 'future'];
-    var timeCompare = timeColor.indexOf(target);
-
-    for (var i = 0; i < timeColor.length; i++) {
-        if (i === timeCompare) {
-            element.toggleClass(timeCompare[i], true);
-        } 
-        else {
-            element.toggleClass(timeCompare[i], false);
+        var timeString = '';
+       
+        var hour = i % 24; 
+        if (hour < 12) {
+            timeString = hour + "AM";
+        } else if (hour == 12) {
+            timeString = hour + "PM";
+        } else {
+            timeString = (hour - 12) + "PM";
         }
+
+       
+        var timeLabel = $("<timeLabel>");
+        timeLabel.attr("class", "col-1 hour py-3");
+        timeLabel.text(timeString);
+        
+        var taskInput = $("<textarea>");
+        taskInput.attr({
+            'class': "col-10",
+            'id': timeString,
+            'data-start': hour, 
+        });
+
+        timeLabel.attr("for", taskInput.attr('id'));
+    
+        taskInput.val(localStorage.getItem(taskInput.attr('id'))); 
+
+        var saveButton = $('<button>');
+        saveButton.attr("class", "col-1 saveButton");
+
+        saveButton.html("<i class='fa fa-save'></i>")
+
+        liElement.append(timeLabel).append(taskInput).append(saveButton);  
+      
+        saveButton.click(function(e) {buttonSave(e)});
     }
 }
 
-function taskTime() {
+function timeFxn() {
+    
     todaysDate = time.format('dddd, MMMM DD');
     $('#currentDay').text(todaysDate);
-
+    
+     
     $('textarea').each(function() {
-        if ($(this).attr('data-start') < time.hour()) {
-            changeColor($(this), 'past');
-        }
-        else if ($(this).attr('data-start') == time.hour()) {
-            changeColor($(this), 'present');
+        if ($(this).attr('data-start') < time.hour()) { 
+            changeColor($(this), 'past');     
+        }  else if ($(this).attr('data-start') == time.hour()) {
+            changeColor($(this), 'present'); 
         } else {
-            changeColor($(this), 'future')
+            changeColor($(this), 'future');
         }
     });
 }
 
-function buttonSave(event) {
-    var element = $(event.target);
-    if (element.is ("i")) {
-        element = element.parent();
+
+function changeColor(element, target) {
+    var timeColor = ['past', 'present','future'];
+    var timeCompare = timeColor.indexOf(target);
+
+    for (var i = 0; i < timeColor.length; i++) {
+        if (i === timeCompare) {
+            element.toggleClass(timeColor[i], true);
+        } else {
+            element.toggleClass(timeColor[i], false);
+        }
     }
-    var inputtedTask = element.prev();
-    var key = inputtedTask.attr("id");
-    var value = inputtedTask.val();
-    localStroage.setItem(key, value);
 }
 
-$(document).ready(function() {
-    divGenerate();
-    taskTime();
 
+function buttonSave(event) {
+    var htmlElement = $( event.target );
+    if (htmlElement.is( "i" )) { 
+        htmlElement = htmlElement.parent();
+    }
+    var taskInput = htmlElement.prev();
+    var key = taskInput.attr('id');
+    var value = taskInput.val();
+    localStorage.setItem(key, value);
+}
+
+$( document ).ready(function() {
+    divGenerate(); 
+    timeFxn(); 
     var timer = setInterval(function() {
-        time = moment();
+        time = moment(); 
         var curMin = curTime.minute();
-        refreshTimer = (60 - curMin) % 60;
-        if (refreshTime === 0) {
-            changeColor();
+        minLeftBfRefresh = (60 - curMin) % 60;
+        if (minLeftBfRefresh === 0) {
+            timeFxn();
         }
-    }, 1000 * 60);
+    }, 1000 * 60);  
 });
-
